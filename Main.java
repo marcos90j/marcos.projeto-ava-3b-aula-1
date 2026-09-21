d8f1bc59d990e8a5ddc6c3ce9088ea26e24ca3b6
marcos90j <marcos.furtado.jesus@escola.pr.gov.br>
2026-09-21 11:10:58 +0000

    java

---
+++ Main.java
@@ -0,0 +1,4 @@
+public class Main {
+    public static void main(String[ ] args)
+         system.out.println(“Bom dia”);
+         system.out.println(“Hoje é dia 17/09, estamos na aula de programação”);
---
+++ variaveis.java
@@ -0,0 +1,9 @@
+public  class variaveis
+
+public static void man(String[]args) {
+String nome = "marcos"
+int idade = 17;
+float altura = 1,72;
+system.out.println("Olá, meu nome é"+ nome + "e tenho" + idade + "anos" + "e tambem meço" + altura + "metros de altura")
+
+}
